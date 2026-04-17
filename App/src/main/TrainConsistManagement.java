68075f4cc0835f07415a6646371b990b080d99fc
atulya257 <atulyasrikrishna257@gmail.com>
2026-04-17 23:56:52 +0530

    atulya257 Added LinearSearchForBoogieID

--- App/src/main/TrainConsistManagement.java
+++ App/src/main/TrainConsistManagement.java
@@ -1,7 +1,13 @@
+import java.util.*;
+
 public class TrainConsistManagement {
 
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
+
+        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "Cargo", "Guard"};
+        Arrays.sort(bogieNames);
+        System.out.println("Sorted bogie names: " + Arrays.toString(bogieNames));
 
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG410"};
         String searchKey = "BG309";
