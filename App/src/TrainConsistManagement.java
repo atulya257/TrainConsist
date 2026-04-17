6bf836bef7f8823c844959dfbd698efbc0bffb12
atulya257 <atulyasrikrishna257@gmail.com>
2026-04-17 18:36:17 +0530

    atulya257 Added MapBogieToCapacity

--- App/src/TrainConsistManagement.java
+++ App/src/TrainConsistManagement.java
@@ -4,14 +4,14 @@ public class TrainConsistManagement {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
 
-        LinkedHashSet<String> trainFormation = new LinkedHashSet<>();
+        HashMap<String, Integer> bogieCapacity = new HashMap<>();
 
-        trainFormation.add("Engine");
-        trainFormation.add("Sleeper");
-        trainFormation.add("Cargo");
-        trainFormation.add("Guard");
-        trainFormation.add("Sleeper");
+        bogieCapacity.put("Sleeper", 72);
+        bogieCapacity.put("AC Chair", 78);
+        bogieCapacity.put("First Class", 24);
 
-        System.out.println("Final train formation: " + trainFormation);
+        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
     }
 }
